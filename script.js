89a3a589106037895b229a88fb82d3379b119f35
noheropage <mursepage@gmail.com>
2021-01-13 12:38:01 -0800

    Prompt user to select types of char to use in password and generate master char list

--- script.js
+++ script.js
@@ -6,31 +6,64 @@ function writePassword() {
   // var password = generatePassword();
   var passwordText = document.querySelector("#password");
   var passLength = parseInt(prompt("Enter the desired password length (between 8-128 characters: "));
-  
+
   // verify valid password length input 
   while (passLength < 8 || passLength > 128 || Number.isInteger(passLength) == false) {
     passLength = parseInt(prompt("Choose a password length between 8 and 128 characters: "))
   }
 
-  var passChar = prompt("Choose which character types should be include (must choose one): ")
+  var passChar = prompt("Pick one or more of the following character types to include in the password (lowercase, uppercase, numeric, and/or special characters): ")
+  var passOptions = passChar.split(" ");
+  var optionLength = passOptions.length;
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  var upperCase = lowerCase.toUpperCase()
+  var numbers = "0123456789"
+  var specialChar = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+  var superPassword = "";
+  var realPassword = "";
+
+  for (i = 0; i < optionLength; i++) {
+    if (passOptions[i] === "lowercase") {
+      superPassword += lowerCase;
+    } else if (passOptions[i] === "uppercase") {
+      superPassword += upperCase;
+    } else if (passOptions[i] === "numeric") {
+      superPassword += numbers;
+    } else if (passOptions[i] === "special") {
+      superPassword += specialChar;
+    } else {
+      passChar = prompt("Please select at least one (lowercase, uppercase, numeric, special): ")
+    }
+  }
 
 
+  for (var i=0, n=superPassword.length; i<passLength; i++) {
+    realPassword += superPassword.charAt(Math.floor(Math.random() * n));
+  }
 
-  // console.log(passLength);
-  // console.log(passChar);
+  // verify valid character input 
+  console.log(passOptions);
+  console.log(lowerCase);
+  console.log(upperCase);
+  console.log(specialChar);
+  console.log(superPassword);
+  console.log(realPassword);
+
+
   passwordText.value = password;
 
 }
 
 
 // Criteria
-  // Length (8 - 128 chars)
+// Length (8 - 128 chars)
 
-  // todo char type (lower, upper, #, and/or special)
+// char type (lower, upper, #, and/or special)
 
-  // todo input validation (ensure at least one char type selected)
+// todo input validation (ensure at least one char type selected)
 
-  // todo generate password with criteria
+// todo generate password with criteria
 
 
 
