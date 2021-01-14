495e3c0dcb39383206735a0794dae0b7fd8d9310
noheropage <mursepage@gmail.com>
2021-01-14 15:14:21 -0800

    Validate user input regarding char type

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
   function generatePassword() {
-
-
     var passLength = parseInt(prompt("Enter the desired password length (between 8-128 characters: "));
 
     // reject password lengths that are too short or too long or not a number 
@@ -15,17 +14,34 @@ function writePassword() {
       passLength = parseInt(prompt("Choose a password length between 8 and 128 characters: "))
     }
 
-    var passChar = prompt("Pick one or more of the following character types to include in the password (lowercase, uppercase, numeric, and/or special): ")
-    var passOptions = passChar.split(" ");
+    
+
+    // var validation = false;
+    // console.log(validation); 
+    do {
+      var passChar = prompt("Pick one or more of the following character types to include in the password (lowercase, uppercase, numeric, and/or special): ")
+      // turns user input into an array 
+      var passOptions = passChar.split(" ");
+
+    }
+    while (passOptions.includes("lowercase") === false && passOptions.includes("uppercase") === false && passOptions.includes("numeric") === false && passOptions.includes("special") === false);      // passChar = prompt("Please select at least one (lowercase, uppercase, numeric, special): ")
+      
+
+
     var optionLength = passOptions.length;
+
+    // strings of possible characters for a password 
     var lowerCase = "abcdefghijklmnopqrstuvwxyz"
     var upperCase = lowerCase.toUpperCase()
     var numbers = "0123456789"
     var specialChar = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 
+    // holds all possible characters 
     var superPassword = "";
+    // holds characters that will be chosen at random 
     var realPassword = "";
 
+    // takes user selection and adds related characters to a master string 
     for (i = 0; i < optionLength; i++) {
       if (passOptions[i] === "lowercase") {
         superPassword += lowerCase;
@@ -35,9 +51,7 @@ function writePassword() {
         superPassword += numbers;
       } else if (passOptions[i] === "special") {
         superPassword += specialChar;
-      } else {
-        passChar = prompt("Please select at least one (lowercase, uppercase, numeric, special): ")
-      }
+      } 
     }
 
     // chooses a random character from a master list of possible password characters for each character in the final password 
@@ -45,16 +59,8 @@ function writePassword() {
       realPassword += superPassword.charAt(Math.floor(Math.random() * superPassword.length));
     }
     return realPassword;
-  }
-  // verify valid character input 
-  // console.log(passOptions);
-  // console.log(lowerCase);
-  // console.log(upperCase);
-  // console.log(specialChar);
-  // console.log(superPassword);
-  // console.log(realPassword);
-
-
+  };
+  
   passwordText.value = password;
 
 }
@@ -65,9 +71,9 @@ function writePassword() {
 
 // char type (lower, upper, #, and/or special)
 
-// todo input validation (ensure at least one char type selected)
+// TODO input validation (ensure at least one char type selected)
 
-// todo generate password with criteria
+// generate password with criteria
 
 
 
