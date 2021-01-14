6b01df7e0011e6a82f8270125327293d15286688
noheropage <mursepage@gmail.com>
2021-01-13 17:40:21 -0800

    Reenable generatePassword function and added return value

--- script.js
+++ script.js
@@ -3,52 +3,56 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var passLength = parseInt(prompt("Enter the desired password length (between 8-128 characters: "));
+  function generatePassword() {
 
-  // verify valid password length input 
-  while (passLength < 8 || passLength > 128 || Number.isInteger(passLength) == false) {
-    passLength = parseInt(prompt("Choose a password length between 8 and 128 characters: "))
-  }
 
-  var passChar = prompt("Pick one or more of the following character types to include in the password (lowercase, uppercase, numeric, and/or special characters): ")
-  var passOptions = passChar.split(" ");
-  var optionLength = passOptions.length;
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
-  var upperCase = lowerCase.toUpperCase()
-  var numbers = "0123456789"
-  var specialChar = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    var passLength = parseInt(prompt("Enter the desired password length (between 8-128 characters: "));
 
-  var superPassword = "";
-  var realPassword = "";
-
-  for (i = 0; i < optionLength; i++) {
-    if (passOptions[i] === "lowercase") {
-      superPassword += lowerCase;
-    } else if (passOptions[i] === "uppercase") {
-      superPassword += upperCase;
-    } else if (passOptions[i] === "numeric") {
-      superPassword += numbers;
-    } else if (passOptions[i] === "special") {
-      superPassword += specialChar;
-    } else {
-      passChar = prompt("Please select at least one (lowercase, uppercase, numeric, special): ")
+    // reject password lengths that are too short or too long or not a number 
+    while (passLength < 8 || passLength > 128 || Number.isInteger(passLength) == false) {
+      passLength = parseInt(prompt("Choose a password length between 8 and 128 characters: "))
     }
+
+    var passChar = prompt("Pick one or more of the following character types to include in the password (lowercase, uppercase, numeric, and/or special): ")
+    var passOptions = passChar.split(" ");
+    var optionLength = passOptions.length;
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+    var upperCase = lowerCase.toUpperCase()
+    var numbers = "0123456789"
+    var specialChar = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+    var superPassword = "";
+    var realPassword = "";
+
+    for (i = 0; i < optionLength; i++) {
+      if (passOptions[i] === "lowercase") {
+        superPassword += lowerCase;
+      } else if (passOptions[i] === "uppercase") {
+        superPassword += upperCase;
+      } else if (passOptions[i] === "numeric") {
+        superPassword += numbers;
+      } else if (passOptions[i] === "special") {
+        superPassword += specialChar;
+      } else {
+        passChar = prompt("Please select at least one (lowercase, uppercase, numeric, special): ")
+      }
+    }
+
+    // chooses a random character from a master list of possible password characters for each character in the final password 
+    for (var i = 0; i < passLength; i++) {
+      realPassword += superPassword.charAt(Math.floor(Math.random() * superPassword.length));
+    }
+    return realPassword;
   }
-
-
-  for (var i=0, n=superPassword.length; i<passLength; i++) {
-    realPassword += superPassword.charAt(Math.floor(Math.random() * n));
-  }
-
   // verify valid character input 
-  console.log(passOptions);
-  console.log(lowerCase);
-  console.log(upperCase);
-  console.log(specialChar);
-  console.log(superPassword);
-  console.log(realPassword);
+  // console.log(passOptions);
+  // console.log(lowerCase);
+  // console.log(upperCase);
+  // console.log(specialChar);
+  // console.log(superPassword);
+  // console.log(realPassword);
 
 
   passwordText.value = password;
